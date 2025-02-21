f5577ad5d87506e396289a44c1e2f6677e9398bc
Manvender <manvender.meena.mat21@itbhu.ac.in>
2025-02-21 17:51:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/ig',(req,res)=>{
+    res.send('<h1>no insta haha</h1>')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
